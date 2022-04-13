cad440ad709934ba744ab6c372c344b4c454ada9
Amo DelBello <2342804+amodelbello@users.noreply.github.com>
2022-04-13 16:58:54 -0600

    gitignore customize.el & settings.el

--- init.el
+++ init.el
@@ -18,4 +18,6 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
 
 (setq custom-file "~/.emacs.d/customize.el")
+(unless (file-exists-p custom-file)
+  (write-region "" "" custom-file))
 (load-file custom-file)
