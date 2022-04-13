9f3d4e9d4a785cee6ed865dfd49df2f78a4d815e
Amo DelBello <2342804+amodelbello@users.noreply.github.com>
2022-04-12 23:05:24 -0600

    Add some packages

--- settings.el
+++ settings.el
@@ -1,10 +1,48 @@
-(use-package yasnippet
+(use-package ace-window
   :ensure t
-  :init
-  (yas-global-mode 1))
+  :config (progn
+	    (defvar aw-dispatch-always nil)
+	    (global-set-key (kbd "C-x o") 'ace-window)
+	    (global-set-key (kbd "s-w") 'ace-window)
+	    (custom-set-faces
+	     '(aw-leading-char-face
+	       ((t (:inherit ace-jump-face-foreground :height 3.0)))))))
 
-(use-package yasnippet-snippets
-  :ensure t)
+(use-package counsel
+  :ensure t
+  :bind
+    (("M-y" . counsel-yank-pop)
+  :map ivy-minibuffer-map
+    ("M-y" . ivy-next-line)))
+
+(use-package ivy
+  :ensure t
+  :diminish (ivy-mode)
+  :bind (("C-x b" . ivy-switch-buffer))
+  :init
+    (ivy-mode 1)
+  :config
+    (setq ivy-use-virtual-buffers t)
+    (setq ivy-count-format "%d/%d ")
+    (setq ivy-display-style 'fancy))
+
+(use-package swiper
+  :ensure t
+  :bind (("C-s" . swiper-isearch)
+	 ("C-r" . swiper-isearch)
+	 ("C-c C-r" . ivy-resume)
+	 ("M-x" . counsel-M-x)
+	 ("C-x C-f" . counsel-find-file))
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq ivy-display-style 'fancy)
+    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)))
+
+(use-package nlinum
+  :ensure t
+  :init (global-nlinum-mode 1))
 
 (use-package org-bullets
   :ensure t
@@ -12,6 +50,19 @@
   (add-hook 'org-mode-hook
 	    (lambda ()
 	      (org-bullets-mode 1))))
+
+(use-package which-key
+  :ensure t 
+  :config
+  (which-key-mode))
+
+(use-package yasnippet
+  :ensure t
+  :init
+  (yas-global-mode 1))
+
+(use-package yasnippet-snippets
+  :ensure t)
 
 (global-set-key (kbd "\e\es")
 		(lambda ()
@@ -45,35 +96,36 @@
 ; easily shrink window vertically
 (global-set-key (kbd "C-x %") (kbd "C-u -1 C-x ^"))
 
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 (setq ibuffer-saved-filter-groups
-           (quote (("default"
-                    ("org" (mode . org-mode))
-                    ("IRC" (or (mode . circe-channel-mode) (mode . circe-server-mode)))
-                    ("web" (or (mode . web-mode) (mode . js2-mode)))
-                    ("shell" (or (mode . eshell-mode) (mode . shell-mode)))
-                    ("mu4e" (or
-                             (mode . mu4e-compose-mode)
-                             (name . "\*mu4e\*")
-                             ))
-                    ("programming" (or
-                                    (mode . emacs-lisp-mode)
-                                    (mode . lisp-mode)
-                                    (mode . clojure-mode)
-                                    (mode . clojurescript-mode)
-                                    (mode . python-mode)
-                                    (mode . c-mode)
-                                    (mode . c++-mode)))
-                    ("text" (mode . text-mode))
-                    ("magit" (mode . magit-mode))
-                    ("dired" (mode . dired-mode))
-                    ("emacs" (or
-                              (name . "^\\*scratch\\*$")
-                              (name . "^\\*Messages\\*$")))
-                    ))))
+	   (quote (("default"
+		    ("org" (mode . org-mode))
+		    ("IRC" (or (mode . circe-channel-mode) (mode . circe-server-mode)))
+		    ("web" (or (mode . web-mode) (mode . js2-mode)))
+		    ("shell" (or (mode . eshell-mode) (mode . shell-mode)))
+		    ("mu4e" (or
+			     (mode . mu4e-compose-mode)
+			     (name . "\*mu4e\*")
+			     ))
+		    ("programming" (or
+				    (mode . emacs-lisp-mode)
+				    (mode . lisp-mode)
+				    (mode . clojure-mode)
+				    (mode . clojurescript-mode)
+				    (mode . python-mode)
+				    (mode . c-mode)
+				    (mode . c++-mode)))
+		    ("text" (mode . text-mode))
+		    ("magit" (mode . magit-mode))
+		    ("dired" (mode . dired-mode))
+		    ("emacs" (or
+			      (name . "^\\*scratch\\*$")
+			      (name . "^\\*Messages\\*$")))
+		    ))))
      (add-hook 'ibuffer-mode-hook
-               (lambda ()
-                 (ibuffer-auto-mode 1)
-                 (ibuffer-switch-to-saved-filter-groups "default")))
+	       (lambda ()
+		 (ibuffer-auto-mode 1)
+		 (ibuffer-switch-to-saved-filter-groups "default")))
 
      ;; Don't show filter groups if there are no buffers in that group
      (setq ibuffer-show-empty-filter-groups nil)
