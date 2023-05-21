c448d1e697b49dd5f818145063ab54e42c09066c
Amo DelBello <amodelbello@protonmail.com>
2023-05-20 23:09:12 -0600

    Remove commented-out theme info in terminal config

--- terminal/settings.el
+++ terminal/settings.el
@@ -2,25 +2,6 @@
 ;; Add `alias emacs="emacs -nw -q --load '~/.emacs.d/terminal/init.el'"`
 ;; to your shell configuration
 
-;; Available Custom Themes:
-;; [ ][ adwaita] -- Face colors similar to the default theme of Gnome 3 (Adwaita).
-;; [ ][ deeper-blue] -- Face colors using a deep blue background.
-;; [ ][ dichromacy] -- Face colors suitable for red/green color-blind users.
-;; [ ][ leuven] -- Face colors with a light background.
-;; [ ][ light-blue] -- Face colors utilizing a light blue background.
-;; [ ][ manoj-dark] -- Very high contrast faces with a black background.
-;; [ ][ misterioso] -- Predominantly blue/cyan faces on a dark cyan background.
-;; [ ][ modus-operandi] -- Accessible and customizable light theme (WCAG AAA standard).
-;; [ ][ modus-vivendi] -- Accessible and customizable dark theme (WCAG AAA standard).
-;; [ ][ tango-dark] -- Face colors using the Tango palette (dark background).
-;; [ ][ tango] -- Face colors using the Tango palette (light background).
-;; [ ][ tsdh-dark] -- A dark theme used and created by Tassilo Horn.
-;; [ ][ tsdh-light] -- A light Emacs theme.
-;; [ ][ wheatgrass] -- High-contrast green/blue/brown faces on a black background.
-;; [ ][ whiteboard] -- Face colors similar to markers on a whiteboard.
-;; [ ][ wombat] -- Medium-contrast faces with a dark gray background.
-;; (load-theme 'deeper-blue t)
-
 (defun on-after-init()
   (unless (display-graphic-p (selected-frame))
     (set-face-background 'default "unspecified-bg" (selected-frame))))
