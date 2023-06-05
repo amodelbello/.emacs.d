c74833f6573d7c0cdb794c9cc9c053d1720fa30a
Amo DelBello <amo.delbello@eyesopen.com>
2023-06-05 10:51:58 -0600

    Re-enable ef-themes

--- init.el
+++ init.el
@@ -15,6 +15,8 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("gnu" . "https://elpa.gnu.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("gnu-devel" . "https://elpa.gnu.org/devel/") t)
 
 (package-initialize)
 
