ee2a1bb20a8da89d8654fefb250da4f18c6dff8b
Amo DelBello <amodelbello@protonmail.com>
2023-08-14 16:48:19 -0700

    Use straight.el instead of package.el

--- init.el
+++ init.el
@@ -1,51 +1,35 @@
+;;
 ;; Turn off mouse interface early in startup to avoid momentary display
+;;
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-;; (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (setq inhibit-startup-message t)
 
-;; Fix bug with missing svg type (should be fixed in emacs 29)
-(setq image-types (cons 'svg image-types))
-
-(eval-when-compile
-  (require 'package)
-  ;;  (require 'diminish)
-  ;;  (require 'bind-key)
-  )
-
-
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("gnu" . "https://elpa.gnu.org/packages/") t)
-;; (add-to-list 'package-archives
-;;              '("gnu-devel" . "https://elpa.gnu.org/devel/") t)
-
-(package-initialize)
-
-(use-package use-package-ensure-system-package
-  :ensure t)
-
-(use-package quelpa
-  :ensure t)
-
-(quelpa
- '(quelpa-use-package
-   :fetcher git
-   :url "https://github.com/quelpa/quelpa-use-package.git"))
-(require 'quelpa-use-package)
-
-;; (custom-set-variables '(epg-gpg-program  "/usr/local/opt/gnupg@2.2/bin/gpg"))
 (epa-file-enable)
 
-;; load environment variables
+;;
+;; straight.el bootstrap
+;;
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)
+
+(use-package use-package-ensure-system-package
+  :straight t)
+
 (use-package dot-env
-  :ensure t
-  :quelpa
-  (dot-env :repo "amodelbello/dot-env.el"
-           :fetcher github :upgrade t)
-  :config
-  (dot-env-config))
+  :straight (:host github :repo "amodelbello/dot-env.el"
+                   :branch "main"))
 
 ;; Set font variables
 (setq amo/font-family (dot-env-get 'FONT "DejaVu Sans Mono")
