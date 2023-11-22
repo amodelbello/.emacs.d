f7b05c2476c13e38e7dae417c0512879fb9ad9ba
Amo DelBello <amodelbello@protonmail.com>
2023-11-21 21:19:53 -0700

    Fix path to customize.el

--- customize-settings.el
+++ customize-settings.el
@@ -1,4 +1,4 @@
-(setq custom-file "~/.emacs.d/terminal/customize.el")
+(setq custom-file (concat user-emacs-directory "customize.el"))
 (unless (file-exists-p custom-file)
   (write-region "" "" custom-file))
 (load custom-file)
