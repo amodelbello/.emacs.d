a861e4fac31bbb180b8f8c5496a9e18628c7c905
Amo DelBello <amo.delbello@eyesopen.com>
2024-10-31 11:48:00 -0600

    Load dot-env in terminal config

--- terminal/init.el
+++ terminal/init.el
@@ -1,7 +1,17 @@
 (load-file (concat user-emacs-directory "common/functions.el"))
 (amo/load-straight (concat user-emacs-directory "terminal/"))
-(amo/load-customize (concat user-emacs-directory "terminal/"))
+
+(use-package use-package-ensure-system-package
+  :straight t)
+
+;; Load dot-env envrionment
+;; https://github.com/amodelbello/dot-env.el
+(use-package dot-env
+  :straight t
+  :config
+  (dot-env-config))
 
 (amo/load-config-file "common/settings.org")
 (amo/load-config-file "terminal/settings.org")
 (amo/load-config-file "common/packages.org")
+(amo/load-customize (concat user-emacs-directory "terminal/"))
